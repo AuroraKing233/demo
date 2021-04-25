87a5915cf376919c6cad896a2da7b9033528bb12
zhoule.zheng <zhoule.zheng@heywind.com>
2021-04-25 09:25:26 +0800

    更改框架

--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Main } from './main.vue'
-// export { default as Drawer } from './Drawer'
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import Vuex from 'vuex'
-
-import user from './module/user'
-import app from './module/app'
-
-export default Vuex.createStore({
-	modules: {user,app}
-})
